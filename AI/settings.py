7c034357eee5d8e7e7111b8951e841360691b1d9
MarcinWojnarowski <wojnarowski1995@gmail.com>
2018-10-24 19:58:24 +0200

    tutorial logowania

--- AI/settings.py
+++ AI/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "emails")
