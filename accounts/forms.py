8e1c190c34879b98cbf32b30ef3530bc16966ea1
MarcinWojnarowski <wojnarowski1995@gmail.com>
2018-11-03 14:55:44 +0100

    listopad

---
+++ accounts/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from accounts.models import MyUser
+
+
+class CustomUserCreationForm(UserCreationForm):     #formularz do tworzenia nowego użytkownika
+   # email = forms.EmailField(required=True)
+
+    class Meta:
+        model = MyUser
+        fields = ('username',
+                  'first_name',
+                  'last_name',
+                  'email',
+                  'password1',
+                  'password2',
+                  )
+
+
+class CustomUserChangeForm(UserChangeForm):   # formularz używany w interfejsie administratora do zmiany informacji i uprawnień użytkownika
+
+    class Meta:
+        model = MyUser
+        fields = ('username',
+                  'first_name',
+                  'last_name',
+                  'email',
+                  )
